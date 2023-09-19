61c1bb7d6fd586bb47aef1c48d6c8cc7a8da3844
Salman-S-Rahman <salmanmern@gmail.com>
2023-09-19 14:16:34 +0600

    all security packages installed

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
+const app = express();
+const hpp = require("hpp");
+const cors = require("cors");
+const helmet = require("helmet");
+const multer = require("multer");
 const express = require("express");
 const router = require("./src/routes/api");
-const app = express();
+const cookieParser = require("cookie-parser");
+const mSanitize = require("mongoose-sanitizer");
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+});
+
+app.use(hpp());
+app.use(cors());
+app.use(limiter);
+app.use(multer());
+app.use(helmet());
+app.use(mSanitize());
+app.use(cookieParser());
+app.use(express.json());
+// all security packages
 
 app.use("/api/v1", router);
+// main route
 
 app.use("*", (req, res) => {
   res.status(404).json({ status: "Fail", data: "Data Not Found" });
